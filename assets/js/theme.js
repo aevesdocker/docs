06a34a506d5aa0bc2498597a975186b82a1a2b26
aevesdocker <allie.sadler@docker.com>
2023-11-07 12:54:12 +0000

    rubbish

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,4 +1,4 @@
-// return 'light' or 'dark' depending on localStorage (pref) or system setting
+// Return 'light' or 'dark' depending on localStorage (pref) or system setting
 function getThemePreference() {
   const theme = localStorage.getItem("theme-preference");
   if (theme) return theme;
@@ -8,13 +8,13 @@ function getThemePreference() {
       : "light";
 }
 
-// update root class based on os setting or localstorage
+// Set the initial theme preference and class on page load
 const preference = getThemePreference();
 document.firstElementChild.className = preference === "dark" ? "dark" : "light";
 localStorage.setItem("theme-preference", preference);
 
-// set innertext for the theme switch button
-// window.addEventListener("DOMContentLoaded", () => {
-//   const themeSwitchButton = document.querySelector("#theme-switch");
-//   themeSwitchButton.textContent = `${preference}_mode`;
-// });
+// Set innertext for the theme switch button
+window.addEventListener("DOMContentLoaded", () => {
+  const themeSwitchButton = document.querySelector("#theme-switch");
+  themeSwitchButton.textContent = `${preference}_mode`;
+});
